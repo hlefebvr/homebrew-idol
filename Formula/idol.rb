6b33dcba445dd93d3910e09eaa33761c5713cecf
Henri Lefebvre <hri.lefebvre@gmail.com>
2026-01-18 23:13:11 +0100

    add formula

---
+++ Formula/idol.rb
@@ -0,0 +1,24 @@
+class Idol < Formula
+  desc "idol: A C++ Framework for Optimization."
+  homepage "https://henrilefebvre.com/idol"
+  on_macos do
+    url "https://github.com/hlefebvr/idol/releases/download/v1.0.0/idol-1.0.0-macos.tar.gz"
+    sha256 "MACOS_SHA256"
+    end
+
+    on_linux do
+    url "https://github.com/hlefebvr/idol/releases/download/v1.0.0/idol-1.0.0-linux.tar.gz"
+    sha256 "LINUX_SHA256"
+    end
+  license "GPL-3.0"
+
+  def install
+    bin.install Dir["bin/*"]          # CLI executables
+    lib.install Dir["lib/*"]          # libraries
+    include.install Dir["include/*"]  # headers
+  end
+
+  test do
+    system "#{bin}/idol_cli", "--version" if File.exist?("#{bin}/idol_cli")
+  end
+end
