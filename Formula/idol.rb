554620b216cbf697b26d26aa850166c8956ae921
Henri Lefebvre <hri.lefebvre@gmail.com>
2026-01-18 23:41:18 +0100

    fix tabs

--- Formula/idol.rb
+++ Formula/idol.rb
@@ -1,24 +1,26 @@
-class Idol < Formula
-  desc "idol: A C++ Framework for Optimization."
-  homepage "https://henrilefebvre.com/idol"
-  on_macos do
-    url "https://github.com/hlefebvr/idol/releases/download/MacOSX/idol-MacOSX-macos.tar.gz"
-    sha256 "a82ba43b8acc792fb906ac32af54a5482e7e4db9cf453c3fc1c0c62be062a432"
+class idol < Formula
+
+    desc "idol: A C++ Framework for Optimization."
+    homepage "https://henrilefebvre.com/idol"
+    
+    on_macos do
+        url "https://github.com/hlefebvr/idol/releases/download/MacOSX/idol-MacOSX-macos.tar.gz"
+        sha256 "a82ba43b8acc792fb906ac32af54a5482e7e4db9cf453c3fc1c0c62be062a432"
     end
 
-    on_linux do
-    url "https://github.com/hlefebvr/idol/releases/download/v1.0.0/idol-1.0.0-linux.tar.gz"
-    sha256 "LINUX_SHA256"
+    #on_linux do
+    #    url "https://github.com/hlefebvr/idol/releases/download/v1.0.0/idol-1.0.0-linux.tar.gz"
+    #    sha256 "LINUX_SHA256"
+    #end
+    license "GPL-3.0"
+
+    def install
+        bin.install Dir["bin/*"]          # CLI executables
+        lib.install Dir["lib/*"]          # libraries
+        include.install Dir["include/*"]  # headers
     end
-  license "GPL-3.0"
 
-  def install
-    bin.install Dir["bin/*"]          # CLI executables
-    lib.install Dir["lib/*"]          # libraries
-    include.install Dir["include/*"]  # headers
-  end
-
-  test do
-    system "#{bin}/idol_cli", "--version" if File.exist?("#{bin}/idol_cli")
-  end
+    test do
+        system "#{bin}/idol_cli", "--version" if File.exist?("#{bin}/idol_cli")
+    end
 end
